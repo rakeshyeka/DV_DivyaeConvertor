797369baa76c74dde1338cc311de8a61deda79b1
rakeshyeka <jayendra.rakesh@gmail.com>
2015-05-12 11:15:01 +0530

    1) Added Sentence aligner and page files

---
+++ src/sentenceAligner/SentenceAligner.java
@@ -0,0 +1,11 @@
+package sentenceAligner;
+
+import java.util.List;
+
+public class SentenceAligner {
+	public static void pageAligner(Page engPage, Page hinPage) {
+		List<String> engBlocks = engPage.getBlocks();
+		List<String> hinBlocks = hinPage.getBlocks();
+
+	}
+}
